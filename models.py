Add Heng unet-resnet34. Fix calculation of iou (wrong metric :D) add badsamples.

models.py
@@ -17,6 +17,20 @@ class ConvBn2d(nn.Module):
         x = self.bn(x)
         return x
 
+class DecoderHeng(nn.Module):
+    def __init__(self, in_channels, channels, out_channels):
+        super(DecoderHeng, self).__init__()
+        self.conv1 = ConvBn2d(in_channels, channels, kernel_size=3, padding=1)
+        self.conv2 = ConvBn2d(channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x, e=None):
+        x = F.upsample(x, scale_factor=2, mode='bilinear', align_corners=True)  # False
+        if e is not None:
+            x = torch.cat([x, e], 1)
+        x = F.relu(self.conv1(x), inplace=True)
+        x = F.relu(self.conv2(x), inplace=True)
+        return x
+
 
 class Decoder(nn.Module):
     def __init__(self, in_channels, channels, out_channels):
@@ -29,6 +43,78 @@ class Decoder(nn.Module):
         x = F.relu(self.conv1(x), inplace=True)
         x = F.relu(self.conv2(x), inplace=True)
         return x
+
+
+class UNetResNet34Heng(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.resnet = torchvision.models.resnet34(pretrained=True)
+
+        self.conv1 = nn.Sequential(
+            self.resnet.conv1,
+            self.resnet.bn1,
+            self.resnet.relu,
+        )  # 64
+        self.encoder2 = self.resnet.layer1  # 64
+        self.encoder3 = self.resnet.layer2  # 128
+        self.encoder4 = self.resnet.layer3  # 256
+        self.encoder5 = self.resnet.layer4  # 512
+
+        self.center = nn.Sequential(
+            ConvBn2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            ConvBn2d(512, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+        )
+
+        self.decoder5 = DecoderHeng(256 + 512, 512, 64)
+        self.decoder4 = DecoderHeng(64 + 256, 256, 64)
+        self.decoder3 = DecoderHeng(64 + 128, 128, 64)
+        self.decoder2 = DecoderHeng(64 + 64, 64, 64)
+        self.decoder1 = DecoderHeng(64, 32, 64)
+
+        self.logit = nn.Sequential(
+            nn.Conv2d(320, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 1, kernel_size=1, padding=0)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        e2 = self.encoder2(x)  # ; print('e2',e2.size())
+        e3 = self.encoder3(e2)  # ; print('e3',e3.size())
+        e4 = self.encoder4(e3)  # ; print('e4',e4.size())
+        e5 = self.encoder5(e4)  # ; print('e5',e5.size())
+
+        f = self.center(e5)
+        d5 = self.decoder5(f, e5)
+        d4 = self.decoder4(d5, e4)
+        d3 = self.decoder3(d4, e3)
+        d2 = self.decoder2(d3, e2)
+        d1 = self.decoder1(d2)
+
+        f = torch.cat((
+            d1,
+            F.upsample(d2,scale_factor=2, mode='bilinear',align_corners=False),
+            F.upsample(d3, scale_factor=4, mode='bilinear', align_corners=False),
+            F.upsample(d4, scale_factor=8, mode='bilinear', align_corners=False),
+            F.upsample(d5, scale_factor=16, mode='bilinear', align_corners=False)
+        ), 1)
+        f = F.dropout2d(f, p=0.5)
+        logit = self.logit(f).view(-1,128,128)
+        # logit = torch.sigmoid(logit)
+        return logit
+
+        # f = self.decoder5(torch.cat([f, e5], 1))  # ; print('d5',f.size())
+        # f = self.decoder4(torch.cat([f, e4], 1))  # ; print('d4',f.size())
+        # f = self.decoder3(torch.cat([f, e3], 1))  # ; print('d3',f.size())
+        # f = self.decoder2(torch.cat([f, e2], 1))  # ; print('d2',f.size())
+
+        # logit = self.logit(f).view(-1, 128, 128)
+        # logit = torch.sigmoid(logit)
+        # return logit
 
 
 class UNetResNet34(nn.Module):
@@ -91,5 +177,5 @@ class UNetResNet34(nn.Module):
         f = self.decoder2(torch.cat([f, e2], 1))  # ; print('d2',f.size())
 
         logit = self.logit(f).view(-1,128,128)
-        logit = F.sigmoid(logit)
+        # logit = torch.sigmoid(logit)
         return logit

multiple_test.py
@@ -15,13 +15,25 @@ from models import UNetResNet34
 from data import TGSSaltDataset, TGSSaltDatasetTest
 from data import get_train_and_validation_samples, get_test_samples
 from data import get_train_and_validation_samples_from_list
-from utils import save_checkpoint
+from utils import save_checkpoint, read_bad_samples
 
 from tensorboardX import SummaryWriter
 
 from augmentation import compute_center_pad
 
 import pandas as pd
+
+def iou_numpy(outputs: np.array, labels: np.array):
+    SMOOTH = 1e-6
+
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+
+    return thresholded  # Or thresholded.mean()
 
 def eval_competition_score():
     DY0, DY1, DX0, DX1 = compute_center_pad(101, 101, factor=32)
@@ -56,23 +68,13 @@ def eval_competition_score():
 
     assert val_masks_stacked.shape == val_predictions_stacked.shape
 
-    from sklearn.metrics import jaccard_similarity_score
+    val_masks_stacked = (val_masks_stacked > 0.5).astype(int)
 
     metric_by_threshold = []
-    for threshold in np.linspace(0, 1, 101):
+    for threshold in np.linspace(0.4, 0.5, 11):
         val_binary_prediction = (val_predictions_stacked > threshold).astype(int)
-
-        iou_values = []
-        for y_mask, p_mask in zip(val_masks_stacked, val_binary_prediction):
-            iou = jaccard_similarity_score(y_mask.flatten(), p_mask.flatten())
-            iou_values.append(iou)
-        iou_values = np.array(iou_values)
-
-        accuracies = [
-            np.mean(iou_values > iou_threshold)
-            for iou_threshold in np.linspace(0.5, 0.95, 10)
-        ]
-        print('Threshold: %.1f, Metric: %.3f' % (threshold, np.mean(accuracies)))
+        accuracies = iou_numpy(val_binary_prediction, val_masks_stacked)
+        print('Threshold: %.2f, Metric: %.5f' % (threshold, np.mean(accuracies)))
         metric_by_threshold.append((np.mean(accuracies), threshold))
 
     best_metric, best_threshold = max(metric_by_threshold)
@@ -123,7 +125,12 @@ def test_submit(threshold):
 
 
 if __name__ == '__main__':
-    use_list = [9, 2, 5, 7]
+    # use_list = [0, 1, 4, 8] -> 822
+    # use_list = [4, 8] -> 819
+    # use_list = [0, 1, 2, 4, 8] -> 823
+    # use_list = [3, 5, 6, 7, 9] -> 825
+    use_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    bad_samples = read_bad_samples()
 
     models = []
 
@@ -138,6 +145,8 @@ if __name__ == '__main__':
                                                                                           validation_list_name)
         for key in samples.keys():
             validation_samples[key] = samples[key]
+
+    list(map(validation_samples.__delitem__, filter(validation_samples.__contains__, bad_samples)))
 
     test_samples = get_test_samples(data_path)
 

test.py
@@ -23,6 +23,18 @@ from augmentation import compute_center_pad
 
 import pandas as pd
 
+def iou_numpy(outputs: np.array, labels: np.array):
+    SMOOTH = 1e-6
+
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+
+    return thresholded  # Or thresholded.mean()
+
 def eval_competition_score():
     DY0, DY1, DX0, DX1 = compute_center_pad(101, 101, factor=32)
     Y0, Y1, X0, X1 = DY0, DY0 + 101, DX0, DX0 + 101
@@ -49,23 +61,13 @@ def eval_competition_score():
 
     assert val_masks_stacked.shape == val_predictions_stacked.shape
 
-    from sklearn.metrics import jaccard_similarity_score
+    val_masks_stacked = (val_masks_stacked > 0.5).astype(int)
 
     metric_by_threshold = []
-    for threshold in np.linspace(0, 1, 11):
+    for threshold in np.linspace(0.4, 0.5, 11):
         val_binary_prediction = (val_predictions_stacked > threshold).astype(int)
-
-        iou_values = []
-        for y_mask, p_mask in zip(val_masks_stacked, val_binary_prediction):
-            iou = jaccard_similarity_score(y_mask.flatten(), p_mask.flatten())
-            iou_values.append(iou)
-        iou_values = np.array(iou_values)
-
-        accuracies = [
-            np.mean(iou_values > iou_threshold)
-            for iou_threshold in np.linspace(0.5, 0.95, 10)
-        ]
-        print('Threshold: %.1f, Metric: %.3f' % (threshold, np.mean(accuracies)))
+        accuracies = iou_numpy(val_binary_prediction, val_masks_stacked)
+        print('Threshold: %.2f, Metric: %.5f' % (threshold, np.mean(accuracies)))
         metric_by_threshold.append((np.mean(accuracies), threshold))
 
     best_metric, best_threshold = max(metric_by_threshold)
@@ -126,7 +128,7 @@ if __name__ == '__main__':
                                                                                           validation_list_name)
     test_samples = get_test_samples(data_path)
 
-    validation_ds = TGSSaltDataset(validation_samples)
+    validation_ds = TGSSaltDataset(validation_samples, phase='validation')
     validation_dl = DataLoader(validation_ds, batch_size=16)
 
     test_ds = TGSSaltDatasetTest(test_samples)
@@ -138,6 +140,7 @@ if __name__ == '__main__':
 
     net = UNetResNet34().cuda()
     net.load_state_dict(checkpoint['state_dict'])
+    net.eval()
 
     score, threshold = eval_competition_score()
     print ('Score: ', score)

train.py
@@ -11,15 +11,28 @@ from torch.optim import lr_scheduler
 import torch.nn.functional as F
 
 from iou_loss import lovasz_hinge, FocalLoss2d
-from models import UNetResNet34
+from models import UNetResNet34, UNetResNet34Heng
 from data import TGSSaltDataset, TGSSaltDatasetTest
 from data import get_train_and_validation_samples, get_test_samples
 from data import get_train_and_validation_samples_from_list
-from utils import save_checkpoint
+from utils import save_checkpoint, read_bad_samples
 
 from tensorboardX import SummaryWriter
 
 from augmentation import compute_center_pad
+
+
+def iou_numpy(outputs: np.array, labels: np.array):
+    SMOOTH = 1e-6
+
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+
+    return thresholded  # Or thresholded.mean()
 
 def eval_competition_score():
     DY0, DY1, DX0, DX1 = compute_center_pad(101, 101, factor=32)
@@ -29,7 +42,7 @@ def eval_competition_score():
     val_masks = []
     for image, mask in validation_dl:
         image = image.cuda()
-        y_pred = net(image).cpu().detach().numpy()
+        y_pred = torch.sigmoid(net(image)).cpu().detach().numpy()
         val_predictions.append(y_pred)
         val_masks.append(mask.detach().numpy())
 
@@ -41,23 +54,15 @@ def eval_competition_score():
 
     assert val_masks_stacked.shape == val_predictions_stacked.shape
 
-    from sklearn.metrics import jaccard_similarity_score
+    # from sklearn.metrics import jaccard_similarity_score
+
+    val_masks_stacked = (val_masks_stacked > 0.5).astype(int)
 
     metric_by_threshold = []
-    for threshold in np.linspace(0, 1, 11):
+    for threshold in np.linspace(0.4, 0.5, 11):
         val_binary_prediction = (val_predictions_stacked > threshold).astype(int)
-
-        iou_values = []
-        for y_mask, p_mask in zip(val_masks_stacked, val_binary_prediction):
-            iou = jaccard_similarity_score(y_mask.flatten(), p_mask.flatten())
-            iou_values.append(iou)
-        iou_values = np.array(iou_values)
-
-        accuracies = [
-            np.mean(iou_values > iou_threshold)
-            for iou_threshold in np.linspace(0.5, 0.95, 10)
-        ]
-        print('Threshold: %.1f, Metric: %.3f' % (threshold, np.mean(accuracies)))
+        accuracies = iou_numpy(val_binary_prediction, val_masks_stacked)
+        print('Threshold: %.2f, Metric: %.5f' % (threshold, np.mean(accuracies)))
         metric_by_threshold.append((np.mean(accuracies), threshold))
 
     best_metric, best_threshold = max(metric_by_threshold)
@@ -77,9 +82,15 @@ if __name__ == '__main__':
     Y0, Y1, X0, X1 = DY0, DY0 + 101, DX0, DX0 + 101
     data_path = os.sep.join(os.getcwd().split(os.sep)[:-1])
 
+    bad_samples = read_bad_samples()
+
 
     criterion = FocalLoss2d()
-    net = UNetResNet34().cuda()
+    # def criterion(logit, truth):
+    #     loss = lovasz_hinge(logit, truth, per_image=True, ignore=None)
+    #     return loss
+
+    net = UNetResNet34Heng().cuda()
     if args.load_weights:
         model_path = osp.join(data_path, args.load_weights)
         checkpoint = torch.load(model_path)
@@ -88,6 +99,9 @@ if __name__ == '__main__':
 
     optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
+    use_list_suffix=''
+    if args.use_list != -1:
+        use_list_suffix = str(args.use_list)
 
     if args.use_list == -1:
         training_samples, validation_samples = get_train_and_validation_samples(data_path)
@@ -95,6 +109,9 @@ if __name__ == '__main__':
         train_list_name = 'list_train'+str(args.use_list)+'_3600'
         validation_list_name = 'list_valid'+str(args.use_list)+'_400'
         training_samples, validation_samples = get_train_and_validation_samples_from_list(data_path, train_list_name, validation_list_name)
+    print (len(bad_samples))
+    list(map(training_samples.__delitem__, filter(training_samples.__contains__, bad_samples)))
+    list(map(validation_samples.__delitem__, filter(validation_samples.__contains__, bad_samples)))
 
     train_ds = TGSSaltDataset(training_samples,phase='train')
     validation_ds = TGSSaltDataset(validation_samples,phase='validation')
@@ -102,8 +119,8 @@ if __name__ == '__main__':
     train_dl = DataLoader(train_ds,batch_size=16,shuffle=True)
     validation_dl = DataLoader(validation_ds,batch_size=8,shuffle=False)
 
-    train_writer = SummaryWriter('logs/train')
-    validation_writer = SummaryWriter('logs/val')
+    train_writer = SummaryWriter('logs/train'+use_list_suffix)
+    validation_writer = SummaryWriter('logs/val'+use_list_suffix)
 
     step = 0
     best_competition_score = 0
@@ -184,7 +201,7 @@ if __name__ == '__main__':
                     'state_dict': net.state_dict(),
                     'best_prec1': comp_score,
                     'optimizer': optimizer.state_dict(),
-                }, data_path, True)
+                }, data_path, True, use_list_suffix)
 
         save_checkpoint({
             'epoch': epoch + 1,
@@ -192,7 +209,7 @@ if __name__ == '__main__':
             'state_dict': net.state_dict(),
             'best_prec1': running_loss,
             'optimizer': optimizer.state_dict(),
-        }, data_path, False)
+        }, data_path, False, use_list_suffix)
 
     print('Finished Training')
 

utils.py
@@ -16,6 +16,11 @@ def get_file_id(filepath):
 
     return id
 
+def read_bad_samples():
+    with open('badsamples.txt', 'r') as file:
+        samples = file.readlines()
+        samples = [sample[:-1] for sample in samples]
+    return samples
 
 def net_compatible_image_size(img):
     height, width, _ = img.shape
@@ -50,8 +55,8 @@ def read_mask(file):
     return mask
 
 
-def save_checkpoint(state, data_path, is_best, filename='checkpoint.pth.tar'):
-    filepath = osp.join(data_path, filename)
+def save_checkpoint(state, data_path, is_best, suffix=''):
+    filepath = osp.join(data_path, 'checkpoint'+suffix+'.pth.tar')
     torch.save(state, filepath)
     if is_best:
-        shutil.copyfile(filepath, osp.join(data_path, 'model_best.pth.tar'))
+        shutil.copyfile(filepath, osp.join(data_path, 'model_best'+suffix+'.pth.tar'))